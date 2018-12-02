b1c8c00dab86628c662df98f74d08b19e71610ef
Kevin Wells <darkoppressor@gmail.com>
2018-12-02 04:32:10 -0600

    Update copyright
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- app/src/main/java/org/cheeseandbacon/shtracker/main/MainAdapter.java
+++ app/src/main/java/org/cheeseandbacon/shtracker/main/MainAdapter.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 Cheese and Bacon Games, LLC
+ * This file is licensed under the MIT License.
+ * See the file docs/LICENSE.txt for the full license text.
+ */
+
 package org.cheeseandbacon.shtracker.main;
 
 import android.content.Context;

33b8b83d504a5161089e9ffde2bb2a15ae7d4b63
Kevin Wells <darkoppressor@gmail.com>
2018-12-13 01:40:31 -0600

    Add action or reason name and severity to day list
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- app/src/main/java/org/cheeseandbacon/shtracker/day/DayAdapter.java
+++ app/src/main/java/org/cheeseandbacon/shtracker/day/DayAdapter.java
@@ -6,7 +6,6 @@
 
 package org.cheeseandbacon.shtracker.day;
 
-import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +13,20 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import org.cheeseandbacon.shtracker.R;
+import org.cheeseandbacon.shtracker.base.BaseActivity;
+import org.cheeseandbacon.shtracker.data.actionTemplate.ActionTemplateLoader;
 import org.cheeseandbacon.shtracker.data.event.Event;
+import org.cheeseandbacon.shtracker.data.reasonTemplate.ReasonTemplateLoader;
 
 import java.util.ArrayList;
 
 public class DayAdapter extends BaseAdapter {
     @NonNull
-    private final Activity activity;
+    private final BaseActivity activity;
     @NonNull
     private final ArrayList<Event> items;
 
-    public DayAdapter (@NonNull Activity activity, @NonNull ArrayList<Event> items) {
+    public DayAdapter (@NonNull BaseActivity activity, @NonNull ArrayList<Event> items) {
         this.activity = activity;
         this.items = items;
     }
@@ -50,7 +52,38 @@ public class DayAdapter extends BaseAdapter {
 
         final Event item = getItem(position);
 
+        TextView textName = convertView.findViewById(R.id.name);
+        TextView textSeverity = convertView.findViewById(R.id.severity);
         TextView textTime = convertView.findViewById(R.id.time);
+
+        if (item.getAction() != null) {
+            textName.setVisibility(View.VISIBLE);
+            textSeverity.setVisibility(View.VISIBLE);
+
+            textSeverity.setText(activity.getString(R.string.day_list_row_severity, item.getAction().getSeverity()));
+
+            ActionTemplateLoader.load(activity, dao ->
+                    dao.getById(item.getAction().getTemplateId()).observe(activity, actionTemplate -> {
+                        if (actionTemplate != null) {
+                            textName.setText(actionTemplate.getName());
+                        }
+                    }));
+        } else if (item.getReason() != null) {
+            textName.setVisibility(View.VISIBLE);
+            textSeverity.setVisibility(View.VISIBLE);
+
+            textSeverity.setText(activity.getString(R.string.day_list_row_severity, item.getReason().getSeverity()));
+
+            ReasonTemplateLoader.load(activity, dao ->
+                    dao.getById(item.getReason().getTemplateId()).observe(activity, reasonTemplate -> {
+                        if (reasonTemplate != null) {
+                            textName.setText(reasonTemplate.getName());
+                        }
+                    }));
+        } else {
+            textName.setVisibility(View.INVISIBLE);
+            textSeverity.setVisibility(View.INVISIBLE);
+        }
 
         textTime.setText(item.getTime());
 

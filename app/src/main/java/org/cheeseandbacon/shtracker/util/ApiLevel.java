fd51152c8c9681e2410eb2ccd75e44a7c7b5a6a1
Kevin Wells <darkoppressor@gmail.com>
2019-08-24 01:16:11 -0400

    Remove unneeded API level checks
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- app/src/main/java/org/cheeseandbacon/shtracker/util/ApiLevel.java
+++ app/src/main/java/org/cheeseandbacon/shtracker/util/ApiLevel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Cheese and Bacon Games, LLC
+ * Copyright (c) 2019 Cheese and Bacon Games, LLC
  * This file is licensed under the MIT License.
  * See the file docs/LICENSE.txt for the full license text.
  */
@@ -9,14 +9,6 @@ package org.cheeseandbacon.shtracker.util;
 import android.os.Build;
 
 public class ApiLevel {
-    static boolean hasVibrationEffects () {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
-    }
-
-    public static boolean hasOnBodySensor () {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
-    }
-
     public static boolean hasUnlockedDeviceOnlyKeyDecryption () {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.P;
     }
--- app/src/main/java/org/cheeseandbacon/shtracker/util/Vibration.java
+++ app/src/main/java/org/cheeseandbacon/shtracker/util/Vibration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Cheese and Bacon Games, LLC
+ * Copyright (c) 2019 Cheese and Bacon Games, LLC
  * This file is licensed under the MIT License.
  * See the file docs/LICENSE.txt for the full license text.
  */
@@ -10,20 +10,18 @@ import android.content.Context;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 
+import static android.os.VibrationEffect.DEFAULT_AMPLITUDE;
+
 public class Vibration {
     // ms
     private static final long BUTTON_PRESS_LENGTH = 5;
-    private static final int BUTTON_PRESS_AMPLITUDE = 72;
+    private static final int BUTTON_PRESS_AMPLITUDE = DEFAULT_AMPLITUDE;
 
     public static void buttonPress (Context context) {
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
         if (vibrator != null && vibrator.hasVibrator()) {
-            if (ApiLevel.hasVibrationEffects() && vibrator.hasAmplitudeControl()) {
-                vibrator.vibrate(VibrationEffect.createOneShot(BUTTON_PRESS_LENGTH, BUTTON_PRESS_AMPLITUDE));
-            } else {
-                vibrator.vibrate(BUTTON_PRESS_LENGTH);
-            }
+            vibrator.vibrate(VibrationEffect.createOneShot(BUTTON_PRESS_LENGTH, BUTTON_PRESS_AMPLITUDE));
         }
     }
 }
